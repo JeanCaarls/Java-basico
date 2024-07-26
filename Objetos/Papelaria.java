71a9a2537905321b5084afcbc273c52423f5f579
JeanCaarls <Carvalhojean8@gmail.com>
2024-07-25 22:58:20 -0300

    Pasta

---
+++ Objetos/Papelaria.java
@@ -0,0 +1,51 @@
+package com.Objetos;
+
+public class Papelaria {
+
+    String Produto;
+    String getProduto() {
+        return Produto;
+    };
+
+    void setProduto(String produto) {
+        Produto = produto;
+    };
+
+    String Marca;
+    String getMarca() {
+        return Marca;
+    };
+
+    void setMarca(String marca) {
+        Marca = marca;
+    };
+
+    String MetodoDePagamento;
+    String getMetodoDePagamento() {
+        return MetodoDePagamento;
+    };
+
+    void setMetodoDePagamento(String metodoDePagamento) {
+        MetodoDePagamento = metodoDePagamento;
+    };
+
+    double Preco;
+    double getPreco() {
+        return Preco;
+    };
+
+    void setPreco(double preco) {
+        Preco = preco;
+    };
+
+    int Qtde;
+    int getQtde() {
+        return Qtde;
+    };
+
+    void setQtde(int qtde) {
+        Qtde = qtde;
+    };
+
+
+}
